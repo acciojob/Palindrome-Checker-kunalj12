97c344d80f48d5afbb3f0f929e4066f83513b0cb
kunal jangle <108586012+kunalj12@users.noreply.github.com>
2023-04-17 14:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	// Remove non-alphanumeric characters and convert to lowercase
+        str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        // Check if the reversed string is equal to the original string
+        return str === str.split('').reverse().join('');
+      }
 
+      let input = prompt("Enter a string to check if it's a palindrome:");
+      let result = isPalindrome(input);
+      alert(`"${input}" is ${result ? '' : 'not '}a palindrome.`);
 }
 module.exports = palindrome
+
